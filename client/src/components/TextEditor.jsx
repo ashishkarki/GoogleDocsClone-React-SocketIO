3d3469b0a3473eabd1292fdcc9a0194c702a8738
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2021-05-02 15:54:06 +0545

    adding server and syncing between server and clients

--- client/src/components/TextEditor.jsx
+++ client/src/components/TextEditor.jsx
@@ -1,10 +1,68 @@
-import { useCallback } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 import Quill from 'quill'
 import 'quill/dist/quill.snow.css'
 
-function TextEditor() {
+import { io } from 'socket.io-client'
+
+const TOOLBAR_OPTIONS = [
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ list: 'ordered' }, { list: 'bullet' }],
+  ['bold', 'italic', 'underline'],
+  [{ color: [] }, { background: [] }],
+  [{ script: 'sub' }, { script: 'super' }],
+  [{ align: [] }],
+  ['image', 'blockquote', 'code-block'],
+  ['clean'],
+]
+
+const TextEditor = () => {
   //const quillWrapperRef = useRef()
+  const [socket, setSocket] = useState()
+  const [quill, setQuill] = useState()
+
+  // we just want this connection from client to server happen once
+  useEffect(() => {
+    const sock = io('http://localhost:3001')
+    setSocket(sock)
+
+    return () => {
+      sock.disconnect()
+    }
+  }, [])
+
+  useEffect(() => {
+    // when this useEffect first fires socket or quill variables might be undefined
+    if (!!!socket === null || !!!quill) return
+
+    const deltaHandler = (delta, oldDelta, source) => {
+      if (source !== 'user') return
+
+      socket.emit('send-changes', delta) // send whatever changed in our client to the server
+    }
+
+    quill.on('text-change', deltaHandler)
+
+    return () => {
+      quill.off('text-change', deltaHandler)
+    }
+  }, [socket, quill])
+
+  useEffect(() => {
+    // when this useEffect first fires socket or quill variables might be undefined
+    if (!!!socket === null || !!!quill) return
+
+    const deltaHandler = delta => {
+      quill.updateContents(delta)
+    }
+
+    socket.on('receive-changes', deltaHandler)
+
+    return () => {
+      socket.off('receive-changes', deltaHandler)
+    }
+  }, [socket, quill])
 
   const quillWrapperRef = useCallback(wrapper => {
     if (wrapper === null) return
@@ -21,7 +79,11 @@ function TextEditor() {
 
     // the reference given to Quill is the editor above so
     // quill puts everything inside this editor which recreated everytime useEffect quits
-    new Quill(editor, { theme: 'snow' })
+    const q = new Quill(editor, {
+      theme: 'snow',
+      modules: { toolbar: TOOLBAR_OPTIONS },
+    })
+    setQuill(q)
 
     // useEffect's cleanup
     // return () => {
---
+++ server/server.js
@@ -0,0 +1,18 @@
+import { Server } from 'socket.io'
+
+const io = new Server(3001, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+})
+
+io.on('connection', socket => {
+  console.log(`connected to client`)
+
+  socket.on('send-changes', delta => {
+    console.log(`delta is ${JSON.stringify(delta)}`)
+
+    socket.broadcast.emit('receive-changes', delta)
+  })
+})
